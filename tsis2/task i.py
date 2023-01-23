03a257a0118a60b89b8158aba8c2cb0ad2067b37
bekonRymkhanov <123162738+bekonRymkhanov@users.noreply.github.com>
2023-01-23 19:48:24 +0600

    Update task i.py

--- tsis2/task i.py
+++ tsis2/task i.py
@@ -1,1 +1,13 @@
-
+import time
+a=input("start loading?(yes or no)")
+b=0
+if(a=="no"):print("it was game of thrones :(")
+elif(a=="yes"):
+    while(b<101):
+        print(b)
+        b+=1
+        time.sleep(0.07)
+    else:
+        print("we loaded a virus to your laptop :) ")
+        time.sleep(1)
+        print("enjoy ")
