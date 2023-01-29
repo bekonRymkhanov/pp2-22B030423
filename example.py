8b7a6f40ae688aee4c93e4fc1ae8c4bf07388448
User <bekarys2004r@gmail.com>
2023-01-29 15:48:22 +0600

    lesser lines

--- example.py
+++ example.py
@@ -7,12 +7,9 @@ while True:
     exam[question]=ans
     next_opper=input("print 'next' to add another question,print 'start' to start a test: ")
     num_of_q+=1
-    if(next_opper=="next"):
-        continue
-    elif(next_opper=="start"):
-        break
-    else:
-        print("i think you mean 'next',so ok.")
+    if(next_opper=="next"):  continue
+    elif(next_opper=="start"): break
+    else: print("i think you mean 'next',so ok.")
 for keys in exam:
     print(keys,": ")
     answer=input()
