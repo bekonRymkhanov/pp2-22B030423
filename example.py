36d4dccd8072432bce9b401f7d26ed0ae4d1f072
User <bekarys2004r@gmail.com>
2023-02-03 17:34:13 +0600

    class clothes

--- example.py
+++ example.py
@@ -1,9 +1,15 @@
-first=list(input())
-second=list()
-for x in first:
-    d=ord(x)
-    if d>=48 and d<=57:
-        second.append(x)
-for i in second:
-    print(i,end="")
-    
+class Clothes:
+    def __init__(self,name, size ,color,price):
+        self.name = name
+        self.size = size
+        self.color = color
+        self.price = price
+
+    def display_info(self):
+        print(f"Name:{self.name}")
+        print(f"size:{self.size}")
+        print(f"color:{self.color}")
+        print(f"price:{self.price}")
+
+Cloth=Clothes(name="asfasd",size=33,color="black",price=3.44)
+Cloth.display_info()
