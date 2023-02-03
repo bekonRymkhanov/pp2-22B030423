830d1eeb07b7b88a47508a68f511801071ae181b
User <bekarys2004r@gmail.com>
2023-02-03 17:53:07 +0600

    nothing

--- example.py
+++ example.py
@@ -1,15 +1,1 @@
-class Clothes:
-    def __init__(self,name, size ,color,price):
-        self.name = name
-        self.size = size
-        self.color = color
-        self.price = price
-
-    def display_info(self):
-        print(f"Name:{self.name}")
-        print(f"size:{self.size}")
-        print(f"color:{self.color}")
-        print(f"price:{self.price}")
-
-Cloth=Clothes(name="asfasd",size=33,color="black",price=3.44)
-Cloth.display_info()
+print(4//2)
