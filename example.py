5cfefa51595830ca5af4fac2e27df1a1d2f0e648
User <bekarys2004r@gmail.com>
2023-02-03 16:32:19 +0600

    nothing

--- example.py
+++ example.py
@@ -1,23 +1,9 @@
-correct=0
-num_of_q=0
-exam={}
-print("hello")
-while True:
-    question=input("question:")
-    ans=input("answer of question:")
-    exam[question]=ans
-    next_opper=input("print 'next' to add another question,print 'start' to start a test: ")
-    num_of_q+=1
-    if(next_opper=="next"):  continue
-    elif(next_opper=="start"): break
-    else: print("i think you mean 'next',so ok.")
-for keys in exam:
-    print(keys,": ")
-    answer=input()
-    if(exam[keys]==answer):
-        correct+=1
-percent=float(correct/num_of_q)*100
-if percent>=70.0:
-    print("congrats you have ",percent," %")
-else:
-    print("you lost")
+first=list(input())
+second=list()
+for x in first:
+    d=ord(x)
+    if d>=48 and d<=57:
+        second.append(x)
+for i in second:
+    print(i,end="")
+    
