5f041422cbd6d75e49e1e096ff9834c5fa4f2fcc
User <bekarys2004r@gmail.com>
2023-02-05 00:56:44 +0600

    Factorial

--- example.py
+++ example.py
@@ -1,18 +1,6 @@
-def isprime(x:int):
-    if x<2:
-        return False
-    for i in range(2,x//2+1):
-        if(x%i==0):
-            return False
-    return True
-first_list=[]
-second_list=[]
-size=int(input("number till:"))
-for i in range(size):
-    if isprime(i):
-        first_list.append(i)
-print(first_list)
-for i in range(1,len(first_list)+1):
-    if isprime(i):
-        print(first_list[i-1],end=",")
-
+def Factorial(x:int):
+    if x==1:
+        return 1
+    return x*Factorial(x-1)
+x=int(input())
+print(Factorial(x))
