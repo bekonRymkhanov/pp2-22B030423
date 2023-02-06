8ac1e5d0acb3bd5b11fd713e2358316746ea1d78
bekonRymkhanov <123162738+bekonRymkhanov@users.noreply.github.com>
2023-02-06 19:28:34 +0600

    Update task 1 xi.py

--- tsis1/task 1 xi.py
+++ tsis1/task 1 xi.py
@@ -1,7 +1,17 @@
-def myFunction() :
-  return True
-
-if myFunction():
-  print("YES!")
-else:
-  print("NO!")
+def isprime(x:int):
+    if x<2:
+        return False
+    for i in range(2,x//2+1):
+        if(x%i==0):
+            return False
+    return True
+first_list=[]
+second_list=[]
+size=int(input("number till:"))
+for i in range(size):
+    if isprime(i):
+        first_list.append(i)
+print(first_list)
+for i in range(1,len(first_list)+1):
+    if isprime(i):
+        print(first_list[i-1],end=",")
