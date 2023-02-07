ccb6467b7e7564a2c364a451e232b8c9b5308d06
bekonRymkhanov <123162738+bekonRymkhanov@users.noreply.github.com>
2023-02-07 22:59:00 +0600

    Update task vi.py

--- tsis2/task vi.py
+++ tsis2/task vi.py
@@ -1,8 +1,3 @@
-my_tuple = tuple(input("write thomthing:"))
-chracters = dict()
-for i in my_tuple:
-    chracters[i]=my_tuple.count(i)
-for i,j in chracters.items():
-    if i==' ':
-        i="space"
-    print(f"{i} character is repeated {j} times")
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
