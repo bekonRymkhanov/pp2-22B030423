6f106ba01450171d8f0d12c5b04b64b2d512f225
User <bekarys2004r@gmail.com>
2023-02-10 18:21:46 +0600

    math.sqrt

--- tsis1/task 1 iii.py
+++ tsis1/task 1 iii.py
@@ -1,5 +1,5 @@
 import math
-if  sqrt(36) > 2:
+if  math.sqrt(36) > 2:
  print("squere root of 36 is greater than two!") 
-if sqrt(16) > 2:
+if math.sqrt(16) > 2:
         print("squere root of 16 is greater than 2") 
