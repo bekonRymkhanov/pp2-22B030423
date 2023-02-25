6fc3951409a2fc88a21e9c159b78cebfad5527f8
User <bekarys2004r@gmail.com>
2023-02-25 20:29:35 +0600

    task9 regex

--- tsis5/regex/task7.py
+++ tsis5/regex/task7.py
@@ -1,7 +1,4 @@
 import re
-import string
-with open(file='row.txt',mode='r',encoding='utf8') as f:
-    rowtxt=f.read()
 text='hello_my_name_is'
 patt='_'
 words=list(re.split(patt,text))
@@ -9,6 +6,3 @@ for  i in range(len(words)):
     words[i]=words[i].capitalize()
 word=''.join(words)
 print(word)
-    
-    
-
