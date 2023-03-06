3039fada20e0ab912ab64289cf165f0d0c8821f4
User <bekarys2004r@gmail.com>
2023-03-06 23:28:15 +0600

    task3

---
+++ tsis6/dir and files/task3.py
@@ -0,0 +1,6 @@
+import os
+pach=("c:/Users/User/Desktop/python/text.txt")
+print(os.path.dirname(pach))
+print(os.path.basename(pach))
+
+
---
+++ tsis6/dir and files/task4.py
@@ -0,0 +1,3 @@
+import os
+with open("c:/Users/User/Desktop/python/tsis1/task1.py",'r') as file:
+    print(file.readlines())
