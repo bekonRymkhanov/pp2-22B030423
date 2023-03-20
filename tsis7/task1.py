99df9181f7be0910245273e23deec839c4ee8ec2
User <bekarys2004r@gmail.com>
2023-03-20 20:24:13 +0600

    actiually finnal mickey

--- tsis7/task1.py
+++ tsis7/task1.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 import datetime
-def hell(a):
+def hell(a,font):
     x,y=0,0
     if abs(a)>360:
         a+=360
@@ -9,19 +9,20 @@ def hell(a):
         pass
     elif abs(a)>90 and abs(a)<=180:
         deg=(a-90)/57.2958
-        x+=math.sin(deg)*220
+        x+=math.sin(deg)*font
     elif abs(a)<=270 and abs(a)>=180:
         deg=(a-180)/57.2958
-        y-=math.sin(deg)*220
-        x+=math.cos(deg)*220
+        y-=math.sin(deg)*font
+        x+=math.cos(deg)*font
     elif abs(a)<=360 and abs(a)>=270:
         deg=(a-270)/57.2958
-        y-=math.cos(deg)*220
+        y-=math.cos(deg)*font
     return x,y
 pygame.init()
 
 clock=pygame.time.Clock()
 height,width=500,500
+font,font1,font2=220,220,125
 screen=pygame.display.set_mode((height,width))
 pygame.display.set_caption("mickey mouse clock")
 
@@ -29,13 +30,13 @@ image=pygame.image.load("better.png")
 reimage=pygame.transform.scale(image,(height+25,width))
 
 secondhands=pygame.image.load("hands.png")
-resecondhands=pygame.transform.scale(secondhands,(220,10))
+resecondhands=pygame.transform.scale(secondhands,(font,10))
 
 minutehands=pygame.image.load("hands.png")
-reminutehands=pygame.transform.scale(minutehands,(220,20))
+reminutehands=pygame.transform.scale(minutehands,(font1,20))
 
 hourhands=pygame.image.load("hands.png")
-rehourhands=pygame.transform.scale(hourhands,(220,30))
+rehourhands=pygame.transform.scale(hourhands,(font2,30))
 runned=True
 
 while runned:
@@ -52,13 +53,13 @@ while runned:
     screen.blit(reimage,(0,0))
 
     rotatedimage=pygame.transform.rotate(resecondhands,a)
-    screen.blit(rotatedimage,(height/2-hell(a)[0],width/2-hell(a)[1]-5))
+    screen.blit(rotatedimage,(height/2-hell(a,font)[0],width/2-hell(a,font)[1]-5))
 
     rotatedsecondhands=pygame.transform.rotate(reminutehands,b)
-    screen.blit(rotatedsecondhands,(height/2-hell(b)[0],width/2-hell(b)[1]-10))
+    screen.blit(rotatedsecondhands,(height/2-hell(b,font1)[0],width/2-hell(b,font1)[1]-10))
 
     rotatedminutehands=pygame.transform.rotate(rehourhands,c)
-    screen.blit(rotatedminutehands,(height/2-hell(c)[0],width/2-hell(c)[1]-15))
+    screen.blit(rotatedminutehands,(height/2-hell(c,font2)[0],width/2-hell(c,font2)[1]-15))
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             runned=False
