2bdc26c2571c61ddbaaa642cbaed5879f63e9d37
User <bekarys2004r@gmail.com>
2023-03-23 21:38:13 +0600

    cringe musicplayer not final

--- examples/example1.py
+++ examples/example1.py
@@ -1,6 +1,9 @@
-import os
-listofmusic=list(os.walk("C:/Users/User/Desktop/python/music"))
-for path,dirs,files in listofmusic:
-    for file in files:
-        pathed=os.path.join("C:/Users/User/Desktop/python/music",file)
-        print(pathed)
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("C:/Users/User/Desktop/python/music/ayaka.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    print("hello")
+while pygame.mixer.music.get_busy() == False:
+    print("finished")
+    break
