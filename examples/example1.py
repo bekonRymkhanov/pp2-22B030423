983614b2699e1be50b782496b6efddecd7b00203
User <bekarys2004r@gmail.com>
2023-03-23 19:36:47 +0600

    somth

--- examples/example1.py
+++ examples/example1.py
@@ -1,2 +1,6 @@
-import datetime
-print(datetime.datetime.now().hour*15)
+import os
+listofmusic=list(os.walk("C:/Users/User/Desktop/python/music"))
+for path,dirs,files in listofmusic:
+    for file in files:
+        pathed=os.path.join("C:/Users/User/Desktop/python/music",file)
+        print(pathed)
--- tsis7/task2.py
+++ tsis7/task2.py
@@ -1,3 +1,17 @@
-import datetime
-a=datetime.datetime.now().second
-print(a)
+import pygame
+import os
+pygame.mixer.init(44100, -16, 2, 4096)
+screen=pygame.display.set_mode((500,500))
+runned=True
+clock=pygame.time.Clock()
+print(pygame.mixer.get_busy())
+while runned:
+    screen.fill((0,0,0))
+    buttonnext=pygame.key.get_pressed()
+    if buttonnext[pygame.K_SPACE]:
+        a=pygame.mixer.music.load('C:/Users/User/Desktop/python/music/somthing.mp3')
+        pygame.mixer.music.play()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            runned=False
+    pygame.display.flip()
